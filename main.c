c52a70ed803eb3f676f6cda16454525a376081ff
doubojv <100632972+doubojv@users.noreply.github.com>
2022-04-30 16:31:38 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,137 +1,98 @@
-#include <raylib.h>
 #include <stdio.h>
-#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include "raylib.h"
 
-float d2(Vector2 a, Vector2 b) {
-    return ((a.x - b.x) * (a.x - b.x)) + ((a.y - b.y) * (a.y - b.y));
-}
+typedef enum GameScreen { LOGO = 0, TITLE, GAMEPLAY, ENDING } GameScreen;
 
-int main() {
-    SetConfigFlags(FLAG_VSYNC_HINT);
-    InitWindow(0, 0, "raylib [core] example - mouse input");
-    if (!IsWindowFullscreen()) ToggleFullscreen();
+int main(){
+    int eixoX = 800, eixoY = 600;
+    Vector2 posicao = {1000, 50 };
+    GameScreen currentScreen = LOGO;
+    Rectangle boxA = { 10, GetScreenHeight()/2.0f - 50, 200, 100 };
+    int boxASpeedX = 4;
+    bool pause = false;             
+    bool collision = false;
 
-    const int screenWidth = GetScreenWidth();
-    const int screenHeight = GetScreenHeight();
+    InitWindow(eixoX, eixoY, "Ulala");
+    SetTargetFPS(90);
 
-    const float W2 = screenWidth / 2.0f;
-    const float H2 = screenHeight / 2.0f;
+    Texture fundo = LoadTexture("assets/fundinho.png");
+    Texture fundoI = LoadTexture("assets/cyberpunk_street.png");
+    Texture fundoF = LoadTexture("assets/lindofundo.png");
+    currentScreen = TITLE;
 
-    Texture ff_img = LoadTexture("assets/ff.png");
-    Texture chico_img = LoadTexture("assets/chico1.png");
+    while (!WindowShouldClose()){
+        if (IsKeyDown(KEY_RIGHT)) posicao.x += 2.0f;
+        if (IsKeyDown(KEY_LEFT)) posicao.x -= 2.0f;
+        if (IsKeyDown(KEY_UP)) posicao.y -= 2.0f;
+        if (IsKeyDown(KEY_DOWN)) posicao.y += 2.0f;
 
-    Sound song = LoadSound("assets/yes-baby.wav");
-    InitAudioDevice();
-    SetMasterVolume(0.2f);
-    int playing = 0;
-
-
-    printf("%f, %f\n", W2, H2);
-
-    Vector2 player = {.x = 10.0f, .y = 10.0f};
-    const float pspeed = 250.0f;
-    Camera2D camera = {.offset = {screenWidth / 2.0f, screenHeight / 2.0f},
-                       .target = {0},
-                       .rotation = 0,
-                       .zoom = 1};
-
-    SetTargetFPS(60);
-
-    double time = GetTime();
-
-    char buf[500] = {0};
-    float dprev = 0.0f;
-
-    while (!WindowShouldClose() && !IsKeyPressed(KEY_CAPS_LOCK)) {
-        double delta = GetTime() - time;
-        time = GetTime();
-
-        if (IsKeyDown(KEY_W)) {
-            player.y -= pspeed * delta;
+        switch(currentScreen) // Tela de fundo
+        {
+            case TITLE:
+            {
+                if (IsKeyPressed(KEY_ENTER)){
+                    currentScreen = GAMEPLAY;
+                }
+            } break;
+            case GAMEPLAY:
+            {
+                if (!pause) boxA.x += boxASpeedX;
+                if (((boxA.x + boxA.width) >= GetScreenWidth()) || (boxA.x <= 0)) boxASpeedX *= -1;
+                if (IsKeyPressed(KEY_ENTER)){
+                    currentScreen = ENDING;
+                }
+            } break;
+            case ENDING:
+            {
+                if (IsKeyPressed(KEY_ENTER)){
+                    currentScreen = TITLE;
+                }
+            } break;
+            default: 
+            break;
         }
-
-        if (IsKeyDown(KEY_S)) {
-            player.y += pspeed * delta;
-        }
-
-        if (IsKeyDown(KEY_D)) {
-            player.x += pspeed * delta;
-        }
-
-        if (IsKeyDown(KEY_A)) {
-            player.x -= pspeed * delta;
-        }
-
-        float camd =
-            d2(camera.target, (Vector2){.x = player.x, .y = player.y}) / 100.0f;
-        float dDelta = abs(camd - dprev);
-
-        float err = camd + (dDelta * 10.0f);
-
-        if (err > 80.0f) {
-            camera.target.x += ((player.x) - camera.target.x) * 0.005;
-            camera.target.y += ((player.y) - camera.target.y) * 0.005;
-        }
-        // LOL 2
-        dprev = camd;
-
         BeginDrawing();
-        ClearBackground(RAYWHITE);
 
-        sprintf(buf, "%02.02f, cam: (%02.02f, %02.0f2), p (%02.02f, %02.02f)",
-                err, camera.target.x, camera.target.y, player.x, player.y);
-        if(playing) DrawText("Yes baby", 10, 50, 15, RED);
-        DrawText(buf, 10, 10, 20, DARKGRAY);
+            ClearBackground(RAYWHITE);
 
-        BeginMode2D(camera);
+            switch(currentScreen) // Tela de fundo
+            {
+                case TITLE:
+                {
+                    DrawTexture(fundoI, 10, 10, WHITE);
+                    DrawText("Pau neles", 850, 20, 40, DARKGREEN);
+                    DrawText("Para começar aperte o enter", 800, 220, 20, DARKGREEN);
 
+                } break;
+                case GAMEPLAY:
+                {
+                    DrawTexture(fundo, 10, 10, WHITE);
+                    DrawCircleV(posicao, 50, MAROON);
+                    //DrawRectangleRec(boxCollision, LIME);
 
-        DrawCircle(0, 0, 10, BLUE);
-        DrawRectangle(player.x, player.y, 40, 40, RED);
+                } break;
+                case ENDING:
+                {
+                    DrawTexture(fundoF, 10, 10, WHITE);
+                    DrawText("Morreu? NOOB!!!", 830, 20, 40, DARKBLUE);
+                    DrawText("Para voltar a tela inicial aperte o enter", 790, 220, 20, DARKBLUE);
 
-        DrawText(
-            "Este círculo azul é o centro do mundo.\nUse WASD para se "
-            "movimentar...",
-            10, 10, 20, DARKGRAY);
-
-        DrawText("Este mundo é a sua tela, o código é a sua tinta.", 700, 10,
-                 20, DARKGRAY);
-
-
-        DrawTexture(ff_img, 1400, 100, WHITE);
-        DrawText("Você pode usar imagens.", 1400, 10,
-                 20, DARKGRAY);
-
-
-        int par = ((int)GetTime())%2;
-        DrawTextureRec(chico_img, (Rectangle) {256 * par, 0, 256, 256 }, (Vector2){2100, 100}, WHITE);
-        DrawText("Animações???", 2100, 10,
-                 20, DARKGRAY);
-
-        if(!playing && player.x > 1800) {
-            PlaySound(song);
-            playing = 1;
-        }
-        DrawText("Música?", 2700, 10,
-                 20, DARKGRAY);
+                } break;
+                default: break;
+            }
 
 
-        DrawText("Projeto de IP OwO", 3200, 10,
-                 40, BLACK);
-
-        DrawText("Estamos ansiosos para ver\no que vocês vão criar.", 3200, 70,
-                 30, BLUE);
-
-
-        EndMode2D();
+        ClearBackground(BLACK);
         EndDrawing();
     }
 
-    UnloadTexture(chico_img);
-    UnloadTexture(ff_img);
 
-    CloseAudioDevice();
+
+    UnloadTexture(fundo);
+    UnloadTexture(fundoI);
+    UnloadTexture(fundoF);
     CloseWindow();
-
     return 0;
 }
